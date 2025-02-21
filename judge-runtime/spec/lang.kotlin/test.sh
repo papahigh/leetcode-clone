fc2f11da0064c848dcecd07cfd867f626e1f7c88
Nikola P <nikolay.papakha@gmail.com>
2025-02-21 06:36:40 +0300

    update kotlin spec

--- judge-runtime/spec/lang.kotlin/test.sh
+++ judge-runtime/spec/lang.kotlin/test.sh
@@ -5,7 +5,7 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 bash "$DIR/cleanup.sh"
 
-sudo nsjail --config /etc/runtime/kotlin.cfg --bindmount "$DIR:/app" --cwd=/app -- /usr/bin/bash compile.sh
+sudo nsjail --config /etc/runtime/kotlin.cfg --bindmount "$DIR:/app" --time_limit=10 --cgroup_mem_max=512000000 --cwd=/app -- /usr/bin/bash compile.sh
 sudo nsjail --config /etc/runtime/kotlin.cfg --bindmount "$DIR:/app" --cwd=/app -- /usr/bin/bash execute.sh
 
 bash "$DIR/cleanup.sh"
