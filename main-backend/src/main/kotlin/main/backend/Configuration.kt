3d6494f96ad22b0ecb7f2498fd7713c3fcfde86f
Nikola P <nikolay.papakha@gmail.com>
2025-02-22 17:45:02 +0300

    update serde imports

--- main-backend/src/main/kotlin/main/backend/Configuration.kt
+++ main-backend/src/main/kotlin/main/backend/Configuration.kt
@@ -18,8 +18,11 @@ import problem.stub.ProblemRepositoryStub
 @SerdeImport(problem.Problem.Project::class)
 @SerdeImport(problem.Problem.ProjectFile::class)
 @SerdeImport(problem.Problem.ProjectFiles::class)
+@SerdeImport(problem.Problem.ProjectAction::class)
 @SerdeImport(problem.Problem.Language::class)
 @SerdeImport(problem.Resources::class)
+@SerdeImport(problem.resources.Time::class)
+@SerdeImport(problem.resources.Memory::class)
 class SerdeConfig
 
 
