42fc4366c9116177864bd3afe43cff7471602cda
Nikola P <nikolay.papakha@gmail.com>
2025-02-28 16:29:36 +0300

    simplify

--- main-frontend/app/problem-listing/problem-listing-page.tsx
+++ main-frontend/app/problem-listing/problem-listing-page.tsx
@@ -3,24 +3,21 @@ import { ProblemListing } from './problem-listing';
 
 import { api } from '~/shared/api';
 
-import type { Route } from './+types/problem-listing-page';
-
 import lockedProblems from './problem-listing.json';
 
 export async function loader() {
   const unlockedProblems = await api.getAllProblems();
   return {
-    meta: {
-      title: 'LeetCodeClone',
-      description: 'Welcome to LeetCodeClone!',
-    },
     unlockedProblems,
     lockedProblems,
   };
 }
 
-export function meta({ data }: Route.MetaArgs) {
-  return [{ title: data.meta.title }, { name: 'description', content: data.meta.description }];
+export function meta() {
+  return [
+    { title: 'LeetCodeClone' },
+    { name: 'description', content: 'Welcome to LeetCodeClone!' },
+  ];
 }
 
 export default function ProblemListingPage() {
